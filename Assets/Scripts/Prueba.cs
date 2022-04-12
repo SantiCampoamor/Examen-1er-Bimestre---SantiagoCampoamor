0971f4692a24f09041444598c93b4e366ac2d2c1
SantiCampoamor <101577703+SantiCampoamor@users.noreply.github.com>
2022-04-12 09:56:56 -0300

    Prueba a medio hacer

---
+++ Assets/Scripts/Prueba.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+    public int taxis;
+    public int dia;
+    public int dialluvia;
+    public int Litros;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (dia + dialluvia < 5)
+        {
+            Debug.Log("ERROR: Se tienen que ingresar más de 5 dias en total");
+        }
+        else if (dialluvia < 0)
+        {
+            Debug.Log("ERROR: Los dias de lluvia tienen que ser mayores a 0");
+        }
+        else if(dia + dialluvia >= 5)
+        {
+            Debug.Log("Una flota de" + taxis + "unidades trabajando durante" + dia + dialluvia +
+                "días implicará un gasto de" + Litros*130 + "pesos en concepto de combustible");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

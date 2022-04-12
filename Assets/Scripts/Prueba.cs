6aafec0aaae3d1e3ba8f4fdd7dc73482bbbf61d2
SantiCampoamor <101577703+SantiCampoamor@users.noreply.github.com>
2022-04-12 10:34:46 -0300

    ya no anda

--- Assets/Scripts/Prueba.cs
+++ Assets/Scripts/Prueba.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public class Prueba : MonoBehaviour
 {
     public int taxis;
-    public int dia;
+    public int diatotal;
     public int dialluvia;
-    int Litros;
-    int diastotal;
+    
     // Start is called before the first frame update
     // 1 dia = 90km = 6 litros
     // 1 dia lluvia = 110km = 7litros
     void Start()
     {
-        int diatotal = dialluvia + dia;
         if (diatotal < 5)
         {
             Debug.Log("ERROR: Se tienen que ingresar más de 5 dias en total");
@@ -23,15 +21,15 @@ public class Prueba : MonoBehaviour
         {
             Debug.Log("ERROR: Los dias de lluvia tienen que ser mayores a 0");
         }
-        else if(dialluvia > diastotal)
+        else if(dialluvia > diatotal)
         {
-            Debug.Log("ERROR: Los dias de lluvia tienen que ser menos que la suma de los dias normales y con lluvia");
+            Debug.Log("ERROR: No se puede ingresar mas dias de lluvia que el total de dias");
         }
 
         else if(diatotal >= 5)
         {
-            Debug.Log("Una flota de" + taxis + "unidades trabajando durante" + diatotal +
-                "días implicará un gasto de" +  (dia*6) + (dialluvia*7) + "pesos en concepto de combustible");
+            Debug.Log("Una flota de " + taxis + " unidades trabajando durante " + diatotal +
+                "días implicará un gasto de " +  (taxis*(diatotal*6 + dialluvia*7))*130 + " pesos en concepto de combustible");
         }
     }
 
